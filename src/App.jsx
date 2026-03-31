f97e1c5e9a8de78674b6c34187b6e55f282bc3e3
mubashircr7 <callme33gamil.com@gmail.com>
2026-03-31 13:41:41 +0600

    digitools-platform project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './component/navbar/navbar'
+import Hero from './component/hero-section/Hero';
+function App() {
+  return ( 
+    <>
+      <Navbar/> 
+      <Hero/>
+    </>
+
+  );
+  
+}
+
+export default App;
---
+++ src/component/hero-section/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+ const Hero = () => {
+  return (
+    <div className="min-h-[50vh] bg-linear-to-r from-lime-500 to-lime-300">
+     Banner
+    </div>
+  );
+};
+
+export default Hero;
